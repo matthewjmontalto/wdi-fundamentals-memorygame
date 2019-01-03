91c41cd32a495f156e7d65ac12b2b376d65dd64c
Matthew Montalto <matthewmontalto@Matthews-MacBook-Air.local>
2019-01-03 14:55:23 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,20 +7,23 @@ var cards = [
 
 var cardsInPlay = [];
 
-var cardOne = cards[2];
-var cardTwo = cards[3];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
+var checkForMatch = function () {
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You Found a Match!");
 	} else {
 		alert("Sorry, try again.");
 	}
-
 }
+};
+
+var flipCard = function (cardId) {
+
+console.log("User flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
