34d1849c5584f03e1495bc7906bd987a997b1cf5
Matthew Montalto <matthewmontalto@Matthews-MacBook-Air.local>
2019-01-04 15:27:28 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,24 @@
 var cards = [
-"queen",
-"queen",
-"king",
-"king"
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
 ];
 
 var cardsInPlay = [];
@@ -19,9 +35,11 @@ if (cardsInPlay.length === 2) {
 
 var flipCard = function (cardId) {
 
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log("User flipped " + cards[cardId].suit);
+console.log("User flipped " + cards[cardId].cardImage);
 
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 };
 
